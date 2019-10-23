9e42cbb2e8e6ba25689e96275481ec33ab62fe0d
David Góngora <fdgongora@iesmurgi.org>
2019-10-23 15:57:36 +0200

    SQLServer

---
+++ UWP_Data_Access_REST/UWP_Data_Access_SQLSERVER/Models/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_Data_Access_SQLSERVER.Models
+{
+    public class Product : INotifyPropertyChanged
+    {
+        public int ProductID { get; set; }
+        public string ProductCode { get { return ProductID.ToString(); } }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string UnitPriceString { get { return UnitPrice.ToString("######.00"); } }
+        public int UnitsInStock { get; set; }
+        public string UnitsInStockString { get { return UnitsInStock.ToString("#####0"); } }
+        public int CategoryId { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+     
+    }
+}
